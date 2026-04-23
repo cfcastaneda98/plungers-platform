0a6efcff20155a37c7778beb27bd63f5750df35d
Carlos Fernando Castaneda <cfcastaneda.1998@gmail.com>
2026-04-22 21:47:25 -0600

    feat: add page routes for experiences, detail, and apply

---
+++ src/app/apply/page.tsx
@@ -0,0 +1,7 @@
+export default function ApplyPage() {
+  return (
+    <main>
+      <h1>Business Application</h1>
+    </main>
+  )
+}
---
+++ src/app/experiences/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function ExperienceDetailPage() {
+  return (
+    <main>
+      <h1>Experience Detail</h1>
+    </main>
+  )
+}
